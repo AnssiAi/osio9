9f963e9081eb8752bf5157965e6babdc0c176c55
Anssi Äijö <anssi.aijo@edu.karelia.fi>
2024-07-03 12:13:19 +0300

    Patientor backend, step2

--- patientorBackend/build/index.js
+++ patientorBackend/build/index.js
@@ -4,10 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+const allowedOrigins = ["http://localhost:5173"];
+const options = {
+    origin: allowedOrigins,
+};
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use((0, cors_1.default)(options));
 app.use(express_1.default.json());
 const PORT = 3001;
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
     console.log("someone pinged here");
     res.send("pong");
 });
