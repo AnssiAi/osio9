c2713ac8f8caabca75ed888bb19337c256f41763
Anssi Äijö <anssi.aijo@edu.karelia.fi>
2024-07-16 09:45:16 +0300

    9.18

--- flightdiary/src/App.tsx
+++ flightdiary/src/App.tsx
@@ -1,9 +1,11 @@
 import axios from "axios";
+import "./styles.css";
 import { useEffect, useState } from "react";
 import { DiaryEntry } from "./types";
 
 function App() {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [message, setMessage] = useState<string>("");
   const [date, setDate] = useState<string>("");
   const [visibility, setVisibility] = useState<string>("");
   const [weather, setWeather] = useState<string>("");
@@ -31,8 +33,17 @@ function App() {
       .post<DiaryEntry>("http://localhost:3000/api/diaries", entryObject)
       .then(response => {
         setDiaries(diaries.concat(response.data));
+      })
+      .catch(err => {
+        if (axios.isAxiosError(err)) {
+          setMessage(err.response?.data);
+          setTimeout(() => {
+            setMessage("");
+          }, 5000);
+        } else {
+          console.log(err);
+        }
       });
-
     setDate("");
     setVisibility("");
     setWeather("");
@@ -42,41 +53,52 @@ function App() {
   return (
     <>
       <h2>Add new entry</h2>
-      <form onSubmit={e => newDiaryHandler(e)}>
-        <label>
-          Date:
-          <input
-            type="text"
-            name="date"
-            onChange={e => setDate(e.target.value)}
-          />
-        </label>
-        <label>
-          Visibility:
-          <input
-            type="text"
-            name="visibility"
-            onChange={e => setVisibility(e.target.value)}
-          />
-        </label>
-        <label>
-          Weather:
-          <input
-            type="text"
-            name="weather"
-            onChange={e => setWeather(e.target.value)}
-          />
-        </label>
-        <label>
-          Comment:
-          <input
-            type="text"
-            name="comment"
-            onChange={e => setComment(e.target.value)}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
+      {message !== "" ? (
+        <div id="message">
+          <p>{message}</p>
+        </div>
+      ) : null}
+      <div>
+        <form onSubmit={e => newDiaryHandler(e)}>
+          <label>
+            Date:
+            <input
+              type="text"
+              name="date"
+              value={date}
+              onChange={e => setDate(e.target.value)}
+            />
+          </label>
+          <label>
+            Visibility:
+            <input
+              type="text"
+              name="visibility"
+              value={visibility}
+              onChange={e => setVisibility(e.target.value)}
+            />
+          </label>
+          <label>
+            Weather:
+            <input
+              type="text"
+              name="weather"
+              value={weather}
+              onChange={e => setWeather(e.target.value)}
+            />
+          </label>
+          <label>
+            Comment:
+            <input
+              type="text"
+              name="comment"
+              value={comment}
+              onChange={e => setComment(e.target.value)}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
       <h2>Diary entries</h2>
       {diaries.map((diary, index) => (
         <div key={index}>
